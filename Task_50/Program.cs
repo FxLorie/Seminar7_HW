3d04edb1c70fb5115a34be4fb146d6d61a5597dc
Alena <weaterwind@gmail.com>
2023-04-08 17:36:29 +0700

    Add Task 50

---
+++ Task_50/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+            Console.Write($"{matrix[i, j]} \t"); // знак табуляции для более красивого вывода матрицы
+        }
+        Console.WriteLine();
+    }
+}
+
+string FindElement(int[,] matrix, int i0, int j0)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == i0 && j == j0) return Convert.ToString(matrix[i, j]);
+        }
+    }
+    return "Такого числа в массиве нет";
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.Write("Введите i j искомого элемента: ");
+int[] ij = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine(FindElement(matrix, ij[0], ij[1]));
